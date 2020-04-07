cb79bebacf1bcf863611c294a0714c7799696ff0
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-07 08:22:19 +0200

    Improve input lookup

--- src/setup-jdk.ts
+++ src/setup-jdk.ts
@@ -5,17 +5,18 @@ import * as path from 'path';
 async function run() {
   try {
     // Type of release. Either a release version, known as General Availability ("ga") or an Early Access ("ea")
-    const release_type = core.getInput('release_type') || 'ga';
+    const release_type = getInput('release_type', {required: false}) || 'ga';
     // OpenJDK feature release version, example: "8", "11", "13".
-    const javaVersion = core.getInput('java-version', {required: true});
+    const javaVersion = getInput('java-version', {required: true});
     // OpenJDK implementation, example: "hotspot", "openj9".
-    const openjdk_impl = core.getInput('openjdk_impl') || 'hotspot';
+    const openjdk_impl =
+      getInput('openjdk_impl', {required: false}) || 'hotspot';
     // Architecture of the JDK, example: "x64", "x32", "arm", "ppc64", "s390x", "ppc64le", "aarch64", "sparcv9".
-    const arch = core.getInput('architecture', {required: false}) || 'x64';
+    const arch = getInput('architecture', {required: false}) || 'x64';
     // Heap size for OpenJ9, example: "normal", "large" (for heaps >=57 GiB).
-    const heap_size = core.getInput('heap_size', {required: false}) || 'normal';
+    const heap_size = getInput('heap_size', {required: false}) || 'normal';
     // Exact release of OpenJDK, example: "latest", "jdk-11.0.4+11.4", "jdk8u172-b00-201807161800".
-    const release = core.getInput('release') || 'latest';
+    const release = getInput('release', {required: false}) || 'latest';
 
     await installer.getJava(
       release_type,
@@ -31,6 +32,31 @@ async function run() {
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+/**
+ * Gets the value of an input.  The value is also trimmed.
+ * IMPORTANT: Unlike `core.getInput`, this version also searches to variants where the "-" are replaced
+ * by "_".
+ * @param     name     name of the input to get
+ * @param     options  optional. See InputOptions.
+ * @returns   string
+ */
+function getInput(name: string, options: any): string {
+  let val =
+    process.env[
+      `INPUT_${name
+        .replace(/ /g, '_')
+        .replace('-', '_')
+        .toUpperCase()}`
+    ] || '';
+  if (!val) {
+    val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+  }
+  if (options && options.required && !val) {
+    throw new Error(`Input required and not supplied: ${name}`);
+  }
+  return val.trim();
 }
 
 run();
